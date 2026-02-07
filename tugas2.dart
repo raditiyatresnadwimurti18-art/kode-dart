56f81a141d2b76fcc9e4fa029261d9460524e7a8
raditiyatresnadwimurti18-art <raditya.tresna.dwimurti18@gmail.com>
2026-02-07 12:38:56 +0700

    revisi1

--- tugas2.dart
+++ tugas2.dart
@@ -1,9 +1,19 @@
 
-void main(){
- stdout.write("Masukan nilai UTS:");
- int nilai_uts = int.parse(stdin.readLineSync()!);
- stdout.write("Masukan nilai UAS:");
- int nilai_uas = int.parse(stdin.readLineSync()!);
- double rata_rata = (nilai_uts + nilai_uas) / 2;
- stdout.writeln("Rata-rata nilai: $rata_rata");
+void main() {
+  // 1. Mengambil input nilai UTS
+  stdout.write("Masukkan nilai UTS: ");
+  String? inputUts = stdin.readLineSync();
+  double uts = double.parse(inputUts ?? '0');
+
+  // 2. Mengambil input nilai UAS
+  stdout.write("Masukkan nilai UAS: ");
+  String? inputUas = stdin.readLineSync();
+  double uas = double.parse(inputUas ?? '0');
+
+  // 3. Menghitung rata-rata
+  double rataRata = (uts + uas) / 2;
+
+  print("\nNilai UTS: $uts");
+  print("Nilai UAS: $uas");
+  print("Rata-rata: $rataRata");
 }
